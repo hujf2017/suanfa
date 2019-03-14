667a75c0b84b9bf83b0a108fa70e450c6fb0b60e
hujf <952405831@qq.com>
2019-03-14 14:03:25 +0800

    递归思想深度应用

--- suanfa-day1/src/com/hd/suanfa/最长路径递归.java
+++ suanfa-day1/src/com/hd/suanfa/最长路径递归.java
@@ -18,55 +18,50 @@ public class ×î³¤Â·¾¶µÝ¹é {
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			int []a ={0,1,0,2,1,0,1,3,2,1,2,1};
-			TreeNode tree =new TreeNode(5);
-			TreeNode tree1 =new TreeNode(4);
-			TreeNode tree2 =new TreeNode(5);
-			TreeNode tree3 =new TreeNode(1);
-			TreeNode tree4 =new TreeNode(1);
-			TreeNode tree5 =new TreeNode(5);
+			int []a ={3,9,20,15,7};
+			TreeNode tree =new TreeNode(1);
+			TreeNode tree1 =new TreeNode(2);
+			TreeNode tree2 =new TreeNode(3);
+			TreeNode tree3 =new TreeNode(4);
+			TreeNode tree4 =new TreeNode(5);
+			//TreeNode tree5 =new TreeNode(5);
 			tree.left =tree1;
-//			tree.right=tree2;
-//			tree1.left=tree3;
-//			tree1.right=tree4;
-//			tree2.right=tree5;
-			System.out.println(longestUnivaluePath(tree));
+			tree.right=tree2;
+			tree2.left=tree3;
+			tree2.right=tree4;
+		//	tree2.right=tree5;
+			System.out.println(maxDepth(tree));
 
 		}
 
-
+		 public static int maxDepth(TreeNode root) {
+	         return  root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right )) + 1;
+	    } 
 		private static int longestUnivaluePath(TreeNode tree) {
-				int i=0;
-				if(tree==null||tree.left==null||tree.right==null){
+				int i=1;
+				if(tree==null){
 					return 0;
 				}else{
 					return calc(tree,i);
 				}			
 		}
-
-
 		private static int calc(TreeNode tree, int x) {
-			int i=x;
-			int j=x;
-			int max=0;
-			if(tree.left!=null){
-				i++;    //
-				max=calc(tree.left,i);	
-				
+				int i=x;
+				int j=x;
+				int max =0;
+				if(tree.left!=null){
+					i++;    //
+					max=calc(tree.left,i);	
+					
+				}
+				if(tree.right!=null){
+					j++;
+					max=calc(tree.right,j);
+					
+				}
+				int k=Math.max(i, j);
+				return 	Math.max(max, k);
 			}
-			if(tree.right!=null){
-				j++;
-				max=calc(tree.right,j);
-				
-			}
-			int k=Math.max(i, j);
-			return 	Math.max(max, k);
-			
-		}
-	   
-	   
-	   
-	   
 }
 
 
